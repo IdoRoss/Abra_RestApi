839d5dd3372706575b611d7ae83da0294f52779d
Ido Rosenberger <idoldo217@gmail.com>
2023-02-06 11:45:57 +0200

    User Annotations

--- Abra_RestApi/Models/User.cs
+++ Abra_RestApi/Models/User.cs
@@ -1,12 +1,17 @@
-﻿namespace Abra_RestApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abra_RestApi.Models
 {
     public class User
     {
+        [Required]
         public int Id { get; set; }
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [RegularExpression("male|female", ErrorMessage = "The Gender must be either 'male' or 'female' only.")]
         public string Gender { get; set; }
-
+        [Range(0, 120)]
         public int Age { get; set; }
     }
 }
